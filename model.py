f8c839f143317a30c6b7955567548272fce55b51
ckwan <brianwan221@gmail.com>
2024-11-06 09:38:10 +0800

    update evaluation

--- model.py
+++ model.py
@@ -27,6 +27,44 @@ class BasicBlock(nn.Module):
 		return out
 
 
+class Lpt2NbodyNetnoperiodicpadding(nn.Module):
+    def __init__(self, block):
+        super(Lpt2NbodyNetnoperiodicpadding, self).__init__()
+        self.layer1 = self._make_layer(block, 3, 64, blocks=2, stride=1)
+        self.layer2 = self._make_layer(block, 64, 128, blocks=1, stride=2)
+        self.layer3 = self._make_layer(block, 128, 128, blocks=2, stride=1)
+        self.layer4 = self._make_layer(block, 128, 256, blocks=1, stride=2)
+        self.layer5 = self._make_layer(block, 256, 256, blocks=2, stride=1)
+        self.deconv1 = nn.ConvTranspose3d(256, 128, 3, stride=2, padding=0)
+        self.deconv_batchnorm1 = nn.BatchNorm3d(num_features=128, momentum=0.1)
+        self.layer6 = self._make_layer(block, 256, 128, blocks=2, stride=1)
+        self.deconv2 = nn.ConvTranspose3d(128, 64, 3, stride=2, padding=0)
+        self.deconv_batchnorm2 = nn.BatchNorm3d(num_features=64, momentum=0.1)
+        self.layer7 = self._make_layer(block, 128, 64, blocks=2, stride=1)
+        self.deconv4 = nn.ConvTranspose3d(64, 3, 1, stride=1, padding=0)
+
+    def _make_layer(self, block, inplanes, outplanes, blocks, stride=1):
+        layers = []
+        for _ in range(blocks):
+            layers.append(block(inplanes, outplanes, stride=stride))
+            inplanes = outplanes
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x1 = self.layer1(x)
+        x = self.layer2(x1)
+        x2 = self.layer3(x)
+        x = self.layer4(x2)
+        x = self.layer5(x)
+        x = nn.functional.relu(self.deconv_batchnorm1(crop_tensor(self.deconv1(x))), inplace=True)
+        x = torch.cat((x, x2), dim=1)
+        x = self.layer6(x)
+        x = nn.functional.relu(self.deconv_batchnorm2(crop_tensor(self.deconv2(x))), inplace=True)
+        x = torch.cat((x, x1), dim=1)
+        x = self.layer7(x)
+        x = self.deconv4(x)
+        return x
+
 class Lpt2NbodyNet(nn.Module):
     def __init__(self, block):
         super(Lpt2NbodyNet, self).__init__()
@@ -66,7 +104,7 @@ class Lpt2NbodyNet(nn.Module):
         x = self.layer7(x)
         x = self.deconv4(x)
         return x
-
+    
 class UNet3D(nn.Module):  
     def __init__(self, block, num_layers=2, base_filters=64, blocks_per_layer=2):
         super(UNet3D, self).__init__()
@@ -149,13 +187,16 @@ class Lpt2NbodyNetLightning(pl.LightningModule):
                 num_layers: int = 4, 
                 base_filters: int = 64, 
                 blocks_per_layer: int = 2, 
+                periodic_padding: bool = True,
                 **kwargs
                 ):
         super(Lpt2NbodyNetLightning, self).__init__()
 
         self.save_hyperparameters(ignore=['kwargs'])  # This will save all init args except kwargs
 
-        if not custom:
+        if not periodic_padding:
+            self.model = Lpt2NbodyNetnoperiodicpadding(BasicBlock)
+        elif not custom:
             self.model = Lpt2NbodyNet(BasicBlock)
         else:
             self.model = UNet3D(block=BasicBlock,num_layers=self.hparams.num_layers,base_filters=self.hparams.base_filters,blocks_per_layer=self.hparams.blocks_per_layer)
--- scripts/c_train_light.sh
+++ scripts/c_train_light.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -l
 
-##SBATCH -p b
-#SBATCH -w b2
+#SBATCH -p c
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
-#SBATCH --gres=gpu:2
-#SBATCH --gpus-per-node=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-gpu=16
-#SBATCH --mem=100G
+#SBATCH --mem=200G
 #SBATCH --output=logs2/d3m-%j.log
 #SBATCH --time=3-00:00:00
 #SBATCH -J "d3m"
@@ -17,6 +16,7 @@ export OMP_NUM_THREADS=${SLURM_CPUS_ON_NODE}
 
 # enable logging    
 export CUDA_LAUNCH_BLOCKING=1.
+
 conda activate d3m
 cd $SLURM_SUBMIT_DIR
-srun python3 /home/user/ckwan1/ml/trainer.py $@ --gpus 2 --num_nodes 1 --num_workers ${SLURM_CPUS_PER_GPU}
+srun python3 /home/user/ckwan1/ml/trainer.py $@ --gpus 1 --num_nodes 1 --num_workers ${SLURM_CPUS_PER_GPU}
